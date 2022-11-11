f0a899de246bfd115b5cc897df48d33772d546f7
Bartosz Żołądź <116107779+baruselski@users.noreply.github.com>
2022-11-11 12:45:42 +0100

    Update main.c

--- main.c
+++ main.c
@@ -9,12 +9,12 @@
 #define Free_space_max 99
 #define Free_space_min 0
 
-uint8_t counter=0; // Liczba wolnych miejsc
+uint8_t counter=0; // Number of free spaces
 
 volatile uint8_t S1_press=0;	
 volatile uint8_t S2_press=0;
 
-void PORTA_IRQHandler(void)	// Podprogram obslugi przerwania od klawiszy S1 i S2
+void PORTA_IRQHandler(void)	// Subprogram for handling S1 i S2
 {
 	uint32_t buf;
 	buf=PORTA->ISFR & (S1_MASK | S2_MASK);
@@ -22,9 +22,9 @@ void PORTA_IRQHandler(void)	// Podprogram obslugi przerwania od klawiszy S1 i S2
 	switch(buf)
 	{
 		case S1_MASK:											
-									if(!(PTA->PDIR&S1_MASK))		// Minimalizacja drgan zestyków						
+									if(!(PTA->PDIR&S1_MASK))		// Minimalisation of contact vibration					
 										{
-										if(!(PTA->PDIR&S1_MASK))	// Minimalizacja drgan zestyków ¨c.d.)
+										if(!(PTA->PDIR&S1_MASK))	// cont.
 										{
 											if(S1_press){
 												S1_press =0;
@@ -37,9 +37,9 @@ void PORTA_IRQHandler(void)	// Podprogram obslugi przerwania od klawiszy S1 i S2
 									break;
 									
 		case S2_MASK:											
-									if(!(PTA->PDIR&S2_MASK))		// Minimalizacja drgan zestyków				
+									if(!(PTA->PDIR&S2_MASK))		// Minimalisation of contact vibration			
 										{
-										if(!(PTA->PDIR&S2_MASK))	// Minimalizacja drgan zestykó¨c.d.)
+										if(!(PTA->PDIR&S2_MASK))	// cont.
 										{
 											if(S2_press){
 												S2_press =0;
@@ -55,32 +55,32 @@ void PORTA_IRQHandler(void)	// Podprogram obslugi przerwania od klawiszy S1 i S2
 										
 		default:			break;
 	}	
-	PORTA->ISFR |=  S1_MASK | S2_MASK;	// Kasowanie wszystkich bitów· ‰SF
+	PORTA->ISFR |=  S1_MASK | S2_MASK;	// Deleton of all bitsÂ·Â â€°SF
 	NVIC_ClearPendingIRQ(PORTA_IRQn);
 }
 
 
 
 int main(){
-	Klaw_Init();								// Inicjalizacja klawiatury
-	Klaw_S1_S2_Int();						// Klawisze S1, S2 zglaszaja przerwanie
+	Klaw_Init();								// Buttons initlalization
+	Klaw_S1_S2_Int();						// S1 and S2 report interrupts
 	
 	while(1){
 	
 		if(S1_press){
-				// funkcje wywolywane po wcisnieciu przycisku S1 (otwarcia szlabanu)
+				// Functions called by S1 (opening the barrier)
 				
 		}
-		if(S2_press){  // Dodatkowa funkcja dla administratora korekta liczby wolnych miejsc 
-									 // Tylko po wcisnieciu S2 mozliwosc zmiany zmiennej "counter" 
+		if(S2_press){  // Additional function for barrier operator (manual display control) 
+									 // After pressing S2, change "counter" value 
 
-				if(!(PTA->PDIR&S3_MASK))	// Sprawdza czy przycisk S3 jest wcisniety (inkrementacja licznika)
+				if(!(PTA->PDIR&S3_MASK))	// Check if S3 is pressed (increment counter)
 				{
 					if(counter<Free_space_max){
 						counter +=1;
 					}
 					}	
-				if(!(PTA->PDIR&S4_MASK))	// Sprawdza czy przycisk S4 jest wcisniety (dekrementacja licznika)
+				if(!(PTA->PDIR&S4_MASK))	// Check if S4 is pressed (decrement counter)
 				{
 					if(counter>Free_space_min){
 					counter -= 1; 
