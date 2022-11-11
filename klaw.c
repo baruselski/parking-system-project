460ca231a5fcffcc30dfae88d327e8c74c2aca66
Bartosz Żołądź <116107779+baruselski@users.noreply.github.com>
2022-11-11 12:40:08 +0100

    Update klaw.c

--- klaw.c
+++ klaw.c
@@ -2,7 +2,7 @@
 
 void Klaw_Init(void)
 {
-	SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK;		// Wlaczenie portu A
+	SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK;		// Turning on port A
 	PORTA->PCR[S1] |= PORT_PCR_MUX(1);
 	PORTA->PCR[S2] |= PORT_PCR_MUX(1);
 	PORTA->PCR[S3] |= PORT_PCR_MUX(1);
@@ -14,7 +14,7 @@ void Klaw_Init(void)
 }
 void Klaw_S1_S2_Int(void)
 {
-	PORTA -> PCR[S1] |= PORT_PCR_IRQC(0xa);		//0xa - zbocze opadające
+	PORTA -> PCR[S1] |= PORT_PCR_IRQC(0xa);		//0xa - falling edge
 	PORTA -> PCR[S2] |= PORT_PCR_IRQC(0xa);		
 	NVIC_ClearPendingIRQ(PORTA_IRQn);
 	NVIC_EnableIRQ(PORTA_IRQn);
