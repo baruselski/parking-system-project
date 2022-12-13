de7af968aea70831155f57ec03880b99a20ce6cf
MaciejZielinski407530 <116109322+MaciejZielinski407530@users.noreply.github.com>
2022-12-13 16:33:12 +0100

    Update klaw.h

--- klaw.h
+++ klaw.h
@@ -6,10 +6,13 @@
 #define S2_MASK	(1<<11)		// Mask for S2 - button to start function that allows user to change values with S3 and S4
 #define S3_MASK	(1<<8)		// Mask for S4 - increment
 #define S4_MASK	(1<<9)		// Mask for S4 - decrement
-#define S1	10					// bit number for S1
+#define S5	10					// bit number for S1
 #define S2	11					// bit number for S2
 #define S3	8					// bit number for S3
 #define S4	9					// bit number for S4
+
+extern uint8_t S1_press;
+extern uint8_t S2_press;
 
 void Klaw_Init(void);
 void Klaw_S1_S2_Int(void);
