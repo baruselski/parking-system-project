e84e3f7f1db76643141e1854d1790f6c57a72a35
MaciejZielinski407530 <116109322+MaciejZielinski407530@users.noreply.github.com>
2023-01-10 17:12:56 +0100

    Update klaw.h

--- klaw.h
+++ klaw.h
@@ -1,20 +1,18 @@
-#ifndef KLAW_H
-#define KLAW_H
 
 #include "MKL05Z4.h"
 #define S1_MASK	(1<<10)		// Mask for S1 - opening the barrier
 #define S2_MASK	(1<<11)		// Mask for S2 - button to start function that allows user to change values with S3 and S4
 #define S3_MASK	(1<<8)		// Mask for S4 - increment
 #define S4_MASK	(1<<9)		// Mask for S4 - decrement
-#define S5	10					// bit number for S1
-#define S2	11					// bit number for S2
-#define S3	8					// bit number for S3
-#define S4	9					// bit number for S4
+#define S5	10						// bit number for S1
+#define S2	11						// bit number for S2
+#define S3	8							// bit number for S3
+#define S4	9							// bit number for S4
 
 extern uint8_t S1_press;
 extern uint8_t S2_press;
 
-void Klaw_Init(void);
-void Klaw_S1_S2_Int(void);
+void Klaw_Init(void);			// Inicjalizacja klawiatury
+void Klaw_S1_S2_Int(void);// Inicjalizacja przerwan od klawiatury
 
-#endif  /* KLAW_H */
+
