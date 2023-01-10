4d345b644215d012e86c65f43a749317956947e9
MaciejZielinski407530 <116109322+MaciejZielinski407530@users.noreply.github.com>
2023-01-10 17:12:41 +0100

    Update serwo.h

--- serwo.h
+++ serwo.h
@@ -1,10 +1,10 @@
 
 
 #include "MKL05Z4.h"
-#define serwo 11
+#define serwo 11				// PIN PWM PORTB 
 
-void serwo_init(void);
-void serwo_open_func(void);
-void serwo_close_func(void);
-void serwo_stop_func(void);
+void serwo_init(void);				// Inicjalizacja PINu dla serwomechanizmu
+void serwo_open_func(void);		// Funkcja otwierajaca
+void serwo_close_func(void);	// Funkcja zamykajaca
+void serwo_stop_func(void);		// Funkcja zatrzymujaca
 
