8c30c6d5b7bcb9904fc733eab2a34a1bd8b093ee
MaciejZielinski407530 <116109322+MaciejZielinski407530@users.noreply.github.com>
2023-01-10 17:13:02 +0100

    Update display.h

--- display.h
+++ display.h
@@ -2,29 +2,19 @@
 #define DISPLAY_H
 
 #include "MKL05Z4.h"
-/*
-#define a_MASK	(1<<2)		//PORTA
-#define b_MASK	(1<<3)		
-#define c_MASK	(1<<4)		
-#define d_MASK	(1<<5)
-#define e_MASK	(1<<6)
-#define f_MASK	(1<<7)
-#define g_MASK	(1<<12)
-#define digit1_MASK	(1<<12) //PORTB
-#define digit2_MASK	(1<<12)
-*/
-#define a	0		// Segments on PORTB			
+
+#define a	0		// PIN number Segments on PORTB			
 #define b	1				
 #define c	2			
-#define d	9		// Sprobowac na B9
+#define d	9		
 #define e	6		 
 #define f	7		
 #define g	8		
-#define digit1	6 		// Digits on PORTA
+#define digit1	6 		// PIN number Digits on PORTA
 #define digit2	7		
 
-void display_init(void);
-void display_func();
+void display_init(void);					// Inicjalizacja wyswietlacza
+void display_func(int n,int seg); // Wyswietlenie podanej cyfry
 
 
 #endif  /* DISPLAY_H */
