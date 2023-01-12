6ef2d52ba980a31649aa9d6da15096e55c9464c2
Bartosz Żołądź <116107779+baruselski@users.noreply.github.com>
2023-01-12 20:27:03 +0100

    Update main.c

--- main.c
+++ main.c
@@ -18,23 +18,24 @@
 
 
 static uint8_t counter=88; 		// Number of free spaces
-static uint8_t D;							// Cyfra czesci dziesiatek
-static uint8_t J;							// Cyfra czesci jednosci
-static uint8_t IN;						// IN = 1 fotokomorka F1 byla przerwana jako pierwsza IN = 0 to F2 byla przerwana jako 1
+static uint8_t D;							// Number of tens
+static uint8_t J;							// Number of ones
+static uint8_t IN;						// IN = 1 photocell F1 was interrupted first 
+								// IN = 0 - F2 was interrupted first
 
 
-void Counter_display(void){						// Wyswietlenie liczby wolnych miejsc na wyswietlaczu
-						D=counter/10;							// Obliczenie cyfry dziesiatek
-						J=counter-(10*D);					// Obliczenie cyfry jednostek
-						display_func(J,1);				// Wyswietlenie cyfry jednostek
+void Counter_display(void){						// Display free spaces on the 7-seg display
+						D=counter/10;							// Calculate number of ones
+						J=counter-(10*D);					// Calculate number of tens
+						display_func(J,1);				// Display ones
 						for(int i=0;i<400;i++){};	// Delay
-						display_func(D,2);				// Wyswietlenie cyfry dziesiatek
+						display_func(D,2);				// Display tens
 						for(int i=0;i<400;i++){};	// Delay
 
 }
-void SysTick_Handler(void)	// Podprogram obslugi przerwania od SysTick'a
+void SysTick_Handler(void)	// Subprogram for handling SysTick interrupts
 { 
-	time++;										// Pomiar czasu od poczatku otwarcia szlabanu, Przerwania co 100ms
+	time++;										// Count time up from the moment of opening the barrier, every 100ms
 }
 
 
@@ -47,89 +48,90 @@ int main(){
 	
 
 	
-	while(1){										// Petla glowna
-		Counter_display();				// Wyswietlenie wartosci counter na wyswietlaczu
+	while(1){										// Main program loop
+		Counter_display();				// Display the counter value 
 
 		if(S1_press){     // Functions called by S1 (opening the barrier)
 			
 			SysTick_Config(SystemCoreClock/10 );	// Start SysTick
 			NVIC_ClearPendingIRQ(PORTB_IRQn);
-			NVIC_EnableIRQ(PORTB_IRQn);						// Start przerwania od fotokomorek		
+			NVIC_EnableIRQ(PORTB_IRQn);						// Start photocell interrupts		
 			
-			serwo_open_func();						//Open start
-			for(int i=0;i<5850;i++){					// Opoznienie sluzace jako czas otwarcia
-				display_func(J,1);							// W trakcie opoznienia wyswietlana jest wartosc na wyswietlaczu
+			serwo_open_func();						// Open start
+			for(int i=0;i<5850;i++){					// Delay used as barrier opening time
+				display_func(J,1);							// During the delay show current value on the display
 				for(int i=0;i<400;i++){};
 				display_func(D,2);
 				for(int i=0;i<400;i++){};
 			}
 			serwo_stop_func();
-																			//Open stop
+																			// Open stop
 															
 																					// Waiting
-					time=0;													// Wyzerowanie czasu
+					time=0;													// Set time to 0
 					while(1){
-						Counter_display();						// Wyswietlenie wartosci counter na wyswietlaczu
-		// (start) Jesli czas otwarcia jest wiekszy od 10s i fotokomorka F1 nie jest przerwana (brak przszkody pod szlabanem)			
+						Counter_display();						// Display counter value
+	
+					// (start) If opening time higher than 10s, photocell F1 is not interrupted (no obstacle under the barrier)
 						if(time>=100&&F1_broken==0){			
-							if(test && F2_broken){					// Sprawdzenie, czy jakikolwiek pojazd przejechal przez obie fotokomorki
-								time=0;												// Wyzerowanie czasu
+							if(test && F2_broken){					// Check if the vehicle drove under both photocells
+								time=0;												// Set time to 0
 									
-									if(IN){														// Jesli IN =1 oznacza, ze F1 przerwana jako pierwsza, czyli pojazd wjechal
-											if(counter>Free_space_min){		// Jesli wartoac counter jest wieksza niz wartosc minimalna
-													counter--;								// nastepuje zmniejszenie liczby wolnych miejsc parkingowych
+									if(IN){														// If IN =1, F1 was interrupted first, so the vehicle drove in
+											if(counter>Free_space_min){		// If counter is higher than min value
+													counter--;								// the amount of parking spots gets lowered
 												}
-												IN=0;												// Wyzerowanie wartosci IN
+												IN=0;												// set IN to 0
 											}
-									else{															// Jesli IN =0 oznacza, ze F2 przerwana jako pierwsza, czyli pojazd wyjechal
-											if(counter<Free_space_max){		// Jesli wartoac counter jest mniejsza niz wartosc maksymalna
-												counter++;									// nastepuje zwiekszenie liczby wolnych miejsc parkingowych
+									else{															// If IN = 0, F2 was interrupted first, so the vehicle drove out
+											if(counter<Free_space_max){		// If counter is lower than max value
+												counter++;									// the amount of parking spots gets increased
 												}
 											}
 											
 								
-								while(1){														// Oczekiwanie 2s po przejezdzie zeby zamknac szlaban
-										if(time>=20&&F1_broken==0){			// Jesli czas jest wiekszy niz 2s i fotokomorka F1 nie jest przerwana (brak przszkody pod szlabanem)
+								while(1){														// Wait 2 seconds before closing
+										if(time>=20&&F1_broken==0){			// If time is bigger than 2s and photocell F1 was not interrupted (no obstacle detected)
 											break;
 											}
-										Counter_display();							// Wyswietlenie wartosci counter na wyswietlaczu
+										Counter_display();							// Display counter value
 										}
 									}
-							time=0;																// Wyzerowanie czasu
-							break;																// Wyjscie z petli przejscie do funkcji zamykajacej szlaban
+							time=0;																// Set time to 0
+							break;																// Exit the loop, go to closing of the barrier
 						}
-		// (stop) Jesli czas otwarcia jest wiekszy od 10s i fotokomorka F1 nie jest przerwana (brak przszkody pod szlabanem)
+		// (stop) If opening time is bigger than 10s and photocell F1 was not interrupted (no obstacle was detected)
 		
-		// (start) Jesli czas otwarcia jest mniejszy niz 10s
-						if(F1_first && F2_broken==0){       		// Jesli F1 jest przerwana jako pierwsza ustawia wartosc IN = 1
+		// (start) If opening time lower than 10s
+						if(F1_first && F2_broken==0){       		// If F1 was interrupted first, set IN = 1
 							IN=1;
 							}
 						
-						if(test && F2_broken){									// Sprawdzenie czy jakis pojazd przejechal (poprawnie przerwane 2 fotokomorki)
-							time=0;																// Wyzerowanie czasu
-							if(IN){																// Jesli IN =1 oznacza, ze F1 przerwana jako pierwsza, czyli pojazd wjechal
-									if(counter>Free_space_min){				// Jesli wartoac counter jest wieksza niz wartosc minimalna
-											counter--;										// nastepuje zmniejszenie liczby wolnych miejsc parkingowych
+						if(test && F2_broken){									// Check if a vehicle passed both photocells correctly
+							time=0;																// Set time to 0
+							if(IN){																// If IN =1, F1 was interrupted first, the vehicle drove in
+									if(counter>Free_space_min){				// If counter is higher than min value
+											counter--;										// the amount of parking spots gets lowered
 											}
-											IN=0;													// Wyzerowanie wartosci IN
+											IN=0;													// Set IN to 0
 										}
-							else{																	// Jesli IN =0 oznacza, ze F2 przerwana jako pierwsza, czyli pojazd wyjechal
-									if(counter<Free_space_max){				// Jesli wartoac counter jest mniejsza niz wartosc maksymalna
-											counter++;										// nastepuje zwiekszenie liczby wolnych miejsc parkingowych
+							else{																	// If IN =0, F2 was interrupted first, the vehicle drove out
+									if(counter<Free_space_max){				// If counter is lower than max value
+											counter++;										// the amount of parking spots gets increased
 											}
 											
 									}
 								
-								while(1){														// Oczekiwanie 2s po przejezdzie zeby zamknac szlaban
-										if(time>=20&&F1_broken==0){			// Jesli czas jest wiekszy niz 2s i fotokomorka F1 nie jest przerwana (brak przszkody pod szlabanem)
+								while(1){														// Wait 2 seconds after the vehicle has passed to close the barrier
+										if(time>=20&&F1_broken==0){			// If time is higher than 2s and photocell F1 was not interrupted (no obstacle detected)
 											break;
 											}
-										Counter_display();							// Wyswietlenie wartosci counter na wyswietlaczu
+										Counter_display();							// Display the counter value
 										}
-							time=0;																// Wyzerowanie czasu
-							break;																// Wyjscie z petli przejscie do funkcji zamykajacej szlaban
+							time=0;																// Set time to 0
+							break;																// Exit the loop
 						}
-			// (stop) Jesli czas otwarcia jest mniejszy niz 10s
+			// (stop) If opening time is lower than 10s
 					}
 		
 						test=0;																	// Wyzerowanie wszystkich zmiennych sygnalizacyjnych
@@ -138,8 +140,8 @@ int main(){
 						F1_broken=0;
 																							// Close start
 							serwo_close_func();
-						for(int i=0;i<6540;i++){					// Opoznienie sluzace jako czas zamykania
-								display_func(J,1);						// W trakcie opoznienia wyswietlana jest wartosc na wyswietlaczu
+						for(int i=0;i<6540;i++){					// Delay used as closing time
+								display_func(J,1);						// During the delay the counter value is displayed
 								for(int i=0;i<400;i++){};
 								display_func(D,2);
 								for(int i=0;i<400;i++){};
@@ -147,9 +149,9 @@ int main(){
 						serwo_stop_func();				
 																							// Close stop
 					
-					S1_press=0;													// Wyzerowanie zmiennej S1_press warunek if(S1_press) wykonuje sie tylko raz
+					S1_press=0;													// Set variable S1 so the if(S1_press) condition happens only once
 					SysTick_Config(1); 									// Stop SysTick
-					NVIC_DisableIRQ(PORTB_IRQn);				// Wylaczenie przerwan z PORTB (fotokomorki)
+					NVIC_DisableIRQ(PORTB_IRQn);				// Turn off PORTB interrupts (photocells)
 					time=0;
 				}
 						
