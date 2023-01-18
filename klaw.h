3de7d04d85531943c6d90320345530740bfc0568
Bartosz Żołądź <116107779+baruselski@users.noreply.github.com>
2023-01-18 11:45:21 +0100

    Update klaw.h

--- klaw.h
+++ klaw.h
@@ -4,7 +4,7 @@
 #define S2_MASK	(1<<11)		// Mask for S2 - button to start function that allows user to change values with S3 and S4
 #define S3_MASK	(1<<8)		// Mask for S4 - increment
 #define S4_MASK	(1<<9)		// Mask for S4 - decrement
-#define S5	10						// bit number for S1
+#define S1	10						// bit number for S1
 #define S2	11						// bit number for S2
 #define S3	8							// bit number for S3
 #define S4	9							// bit number for S4
