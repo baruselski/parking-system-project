038c9c577d4c3504c7f9d720a561e60318f35372
Bartosz Żołądź <116107779+baruselski@users.noreply.github.com>
2023-01-18 11:45:48 +0100

    Update klaw.c

--- klaw.c
+++ klaw.c
@@ -5,18 +5,18 @@ uint8_t S2_press=0;
 void Klaw_Init(void)
 {
 	SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK;		// Turning on port A
-	PORTA->PCR[S5] |= PORT_PCR_MUX(1);
+	PORTA->PCR[S1] |= PORT_PCR_MUX(1);
 	PORTA->PCR[S2] |= PORT_PCR_MUX(1);
 	PORTA->PCR[S3] |= PORT_PCR_MUX(1);
 	PORTA->PCR[S4] |= PORT_PCR_MUX(1);
-	PORTA->PCR[S5] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;  // Attach a pull-up resistor and connect it to VCC
+	PORTA->PCR[S1] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;  // Attach a pull-up resistor and connect it to VCC
 	PORTA->PCR[S2] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
 	PORTA->PCR[S3] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
 	PORTA->PCR[S4] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
 }
 void Klaw_S1_S2_Int(void)
 {
-	PORTA -> PCR[S5] |= PORT_PCR_IRQC(0xa);		//0xa - falling edge
+	PORTA -> PCR[S1] |= PORT_PCR_IRQC(0xa);		//0xa - falling edge
 	PORTA -> PCR[S2] |= PORT_PCR_IRQC(0xa);		
 	NVIC_ClearPendingIRQ(PORTA_IRQn);
 	NVIC_EnableIRQ(PORTA_IRQn);
